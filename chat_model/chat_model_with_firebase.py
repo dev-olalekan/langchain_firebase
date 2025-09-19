88f740f98918d70e9d786cbd03d9b2a81f024bc6
olalekan <olalekanotuyiga@gmail.com>
2025-09-19 10:11:05 +0100

    integrating firestore database with via terminal and gcloud cli

---
+++ chat_model/chat_model_with_firebase.py
@@ -0,0 +1,30 @@
+from google.cloud import firestore
+from langchain_google_firestore import FirestoreChatMessageHistory
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+
+
+"""
+Steps to replicate this example:
+1. Create a Firebase account
+2. Create a new Firebase project and FireStore Database
+3. Retrieve the Project ID
+4. Install the Google Cloud CLI on your computer
+    - https://cloud.google.com/sdk/docs/install
+    - Authenticate the Google Cloud CLI with your Google account
+        - https://cloud.google.com/docs/authentication/provide-credentials-adc#local-dev
+    - Set your default project to the new Firebase project you created
+5. pip install langchain-google-firestore
+6. Enable the Firestore API in the Google Cloud Console:
+    - https://console.cloud.google.com/apis/enableflow?apiid=firestore.googleapis.com&project=crewai-automation
+"""
+
+
+
+
+
+
+
+#set up firebase
+PROJECT_ID = langchainproject-1b5f0
